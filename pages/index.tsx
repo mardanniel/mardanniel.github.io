76ac9d793646efea46f98f0fa5860ccca8785876
mardanniel <mginturo@you-source.com>
2022-11-29 16:28:20 +0800

    update pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,24 @@
+import Layout from '@components/layout';
+import Metadata from '@components/metadata';
+import Navigation from '@components/navigation';
+import About from '@components/sections/about';
+import Contact from '@components/sections/contact';
+import Experience from '@components/sections/experience';
+import Intro from '@components/sections/intro';
+import Work from '@components/sections/work';
 
-export default function Home() {
+export default function Index() {
   return (
-    <div className='font-extrabold'>Hello World</div>
-  )
+    <>
+      <Metadata />
+      <Layout>
+        <Navigation />
+        <Intro />
+        <About />
+        <Experience />
+        <Work />
+        <Contact />
+      </Layout>
+    </>
+  );
 }
