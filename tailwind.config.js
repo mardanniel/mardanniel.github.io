f2f8dfffd0c582cd4f6895f5951cb732fda5e206
mardanniel <mginturo@you-source.com>
2022-12-04 06:23:11 +0800

    update configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('tailwindcss').Config} */
 const { fontFamily } = require('tailwindcss/defaultTheme');
 
@@ -10,6 +11,13 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['var(--font-default)', ...fontFamily.sans],
+      },
+      colors: {
+        'palette-color-a': 'rgb(6, 17, 60)',
+        'palette-color-b': 'rgb(255, 140, 50)',
+        'palette-color-c': 'rgb(221, 221, 221)',
+        'palette-color-d': 'rgb(238, 238, 238)',
+        'palette-color-e': 'rgb(141, 141, 141)',
       },
     },
   },
