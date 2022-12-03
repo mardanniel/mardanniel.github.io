fd865b2a5c60d0fd624454469e5ec347b03f4d35
mardanniel <mginturo@you-source.com>
2022-12-04 06:23:19 +0800

    update types

--- types.ts
+++ types.ts
@@ -1,7 +1,35 @@
+/**
+ * Types
+ */
+
 export type TExperience = {
   position: string;
   company: string;
+  link: string;
   fromDate: string;
   toDate: string | "Present";
   descriptions: string[]
 };
+
+/**
+ * Interfaces
+ */
+
+export interface IOrangeLinkProps
+  extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
+  children?: React.ReactNode;
+  linktype: EOrangeLinkTypes;
+}
+
+export interface weqweq {
+  ewq: string
+}
+
+/**
+ * Enums
+ */
+
+export enum EOrangeLinkTypes {
+  underlined,
+  bordered,
+}
